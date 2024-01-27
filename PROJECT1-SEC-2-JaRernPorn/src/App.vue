1b24275bf198a2c94019a7b983c057acf340edf6
bisbat <baitong.sae-@mail.kmutt.ac.th>
2024-01-28 00:57:18 +0700

    starter vue
    starter

---
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+ 
+<template>
+<div>
+  <h1 class="text-3xl font-bold underline bg-red-500">
+    Hello world!
+  </h1>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
