cee2af9908d0a4bbc569a4917743cc5ef4746739
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-01-31 00:03:48 +0700

    home-page

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -3,12 +3,51 @@
 </script>
  
 <template>
-<div>
-  <h1 class="text-3xl font-bold underline bg-red-500">
-    Hello world!
-  </h1>
-</div>
+  <div class="h-screen bg-main-bgColor p-5">
+    <div
+      class="border-double border-8 border-title box-border h-full w-full flex flex-col"
+    >
+      <img
+        class="w-16 absolute right-12 top-12"
+        src="./assets/home-page/setting.png"
+        alt=""
+      />
+      <div class="logo-title text-center text-title text-title-size pt-title">
+        <div class="font-alkatra font-medium leading-title">HELLO !</div>
+        <div class="font-alkatra font-medium leading-title">CHINESE</div>
+      </div>
+      <div class="sub-title text-center">
+        <p class="font-outfit tracking-sub-title text-sub-title-size">
+          LET'S PRACTICE YOUR CHINESE VOCABULARY
+        </p>
+      </div>
+
+      <div id="home-page-button">
+        <div id="play-button" class="flex justify-center pt-14">
+          <img
+            class="w-72 relative"
+            src="./assets/home-page/red-bible-button.png"
+            alt="play-button"
+          />
+          <p class="absolute font-outfit font-normal text-white text-4xl p-5">
+            PLAY
+          </p>
+        </div>
+        <div id="HTP-button" class="flex justify-center pt-6">
+          <img
+            class="w-72 relative"
+            src="./assets/home-page/red-bible-button.png"
+            alt="HTP-button"
+          />
+          <p class="absolute font-outfit font-normal text-white text-2xl p-6">
+            HOW TO PLAY
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
  
 <style scoped>
 
--- PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
+++ PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
@@ -1,12 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        alkatra: ["Alkatra", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        title: "#B11717",
+        "button-bgColor": "#861616",
+        "main-bgColor": "#FDFFE8",
+        white: "#FFFFFF",
+        "quiz-bgColor": "#F4E4C0",
+        black: "#20201D",
+      },
+      fontSize: {
+        "title-size": "110px",
+        "sub-title-size": "14px",
+      },
+      lineHeight: {
+        title: "1.10",
+      },
+      padding: {
+        title: "42px",
+      },
+      letterSpacing: {
+        "sub-title" : "2.8px",
+      },
+    },backgroundImage: {
+      'chinese-frame' : "url('../assets/chinese-frame.png')",
+    }
   },
   plugins: [],
-}
+};
 
