0031892a289c000817cb3aba40e0c475bc43e2ca
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2024-01-31 22:16:45 +0700

    result v.1

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -1,13 +1,90 @@
-<script setup>
+<script setup></script>
 
-</script>
- 
 <template>
+<<<<<<< Updated upstream
 <div>
 
 </div>
 </template>
  
 <style scoped>
+=======
+  <!-- Word List Page -->
+  <div class="w-full h-screen bg-main-bgColor pt-4 flex justify-center">
+    <div class="flex flex-col justify-center gap-9">
+      <div class="flex flex-col">
+        <header
+          class="font-Alkarta font-semibold text-title text-8xl text-center"
+        >
+          Word List
+        </header>
+        <div
+          class="font-Outfit font-semibold text-lg text-center tracking-title"
+        >
+          You did great! Here are the result.
+        </div>
+      </div>
+>>>>>>> Stashed changes
 
-</style>
+      <div class="flex flex-wrap justify-center gap-10">
+        <section class="flex flex-col">
+          <div
+            class="bg-white shadow-lg font-Outfit font-semibold text-xl p-5 rounded-3xl w-80 h-100"
+          >
+            Your Answer:
+            <div class="border-b-2 border-black"></div>
+            <div
+              class="list font-NotoSansSC font-semibold text-xl tracking-title leading-list"
+            >
+              1. 苹果 píngguǒ <br />
+              2. 西瓜 (xīguā)<br />
+              3. 草莓 (cǎoméi)<br />
+              <span class="text-title">4. 木瓜 (mùguā)</span> <br />
+              5. 芒果 (mángguǒ)<br />
+              6. 香蕉 (xiāngjiāo)<br />
+              7. 葡萄 (pú táo)
+            </div>
+          </div>
+        </section>
+
+        <section class="flex flex-col">
+          <div
+            class="bg-white shadow-lg font-Outfit text-xl font-semibold p-5 rounded-3xl w-80 h-100"
+          >
+            Answer:
+            <div class="border-b-2 border-black"></div>
+            <div
+              class="list font-NotoSansSC font-semibold text-xl tracking-title leading-list"
+            >
+              1. 苹果 píngguǒ <br />
+              2. 西瓜 (xīguā)<br />
+              3. 草莓 (cǎoméi)<br />
+              4. 菠萝 (bōluó) <br />
+              5. 芒果 (mángguǒ)<br />
+              6. 香蕉 (xiāngjiāo)<br />
+              7. 葡萄 (pú táo)
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div class="flex flex-wrap justify-center pt-8">
+        <!-- p-14 -->
+        <div class="">
+          <div
+            class="bg-title rounded-lg font-Outfit font-semibold text-xl p-4 text-white uppercase w-40 text-center hover:bg-button-bgColor mx-5"
+          >
+            Play Again
+          </div>
+        </div>
+        <div
+          class="bg-title rounded-lg font-Outfit font-semibold text-xl p-4 text-white uppercase w-40 text-center hover:bg-button-bgColor mx-5"
+        >
+          Home
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
+++ PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+<<<<<<< Updated upstream
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -7,4 +8,45 @@ export default {
   },
   plugins: [],
 };
+=======
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px', //phone
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px' //desktop
+    },
+    extend: {
+      colors: {
+        title: '#B11717',
+        'button-bgColor': '#861616',
+        'main-bgColor': '#FDFFE8',
+        white: '#FFFFFF',
+        'quiz-bgColor': '#F4E4C0',
+        black: '#20201D'
+      }
+    },
+>>>>>>> Stashed changes
 
+    fontFamily: {
+      Alkarta: ['Alkatra', 'system-ui'],
+      Outfit: ['Outfit', 'sans-serif'],
+      NotoSansSC: ['Noto Sans SC', 'sans-serif']
+    },
+    lineHeight: {
+      title: '1.10',
+      list: '2.5'
+    },
+    // padding: {
+    //   title: '100px'
+    // },
+    letterSpacing: {
+      title: '2.8px'
+    }
+  },
+  backgroundImage: {
+    'chinese-frame': "url('../assets/chinese-frame.png')"
+  },
+  plugins: [[require('daisyui')]]
+}
