4a9165bd7baec5d2d80ba898247e1cc409c478cb
h88ych <hathaitat88@gmail.com>
2024-02-01 00:00:17 +0700

    AddSetting

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -1,13 +1,41 @@
-<script setup>
+<script setup></script>
 
-</script>
- 
 <template>
-<div>
+  <div class="w-full h-screen flex justify-center items-center">
+    <div class="relative max-w-[80%]">
+      <div
+        class="rounded-3xl bg-white bg-opacity-95 w-[450px] h-[403px] border border-1 border-black"
+      >
+        <div class="flex-col p-5">
+          <img
+            src="./assets/cancel.png"
+            alt="setting button"
+            class="absolute right-4 top-5 w-8"
+          />
+          <div
+            class="text-center text-settingButton-size font-alkatra text-title font-semibold"
+            style="text-shadow: 2px 2px 4px rgba(18, 17, 17, 0.5)"
+          >
+            Setting
+          </div>
 
-</div>
+          <div class="flex flex-col items-center gap-1">
+            <img
+              src="./assets/resumeButton.png"
+              alt="resume button"
+              class="resume button mt-1"
+            />
+            <img
+              src="./assets/restartButton.png"
+              alt="restart button"
+              class="mb-2"
+            />
+            <img src="./assets/homebutton.png" alt="home button" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
- 
-<style scoped>
 
-</style>
+<style scoped></style>
--- PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
+++ PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
@@ -1,10 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src//*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        alkatra: ['Alkatra', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif']
+      },
+      colors: {
+        title: '#B11717',
+        'button-bgColor': '#861616',
+        'main-bgColor': '#FDFFE8',
+        white: '#FFFFFF',
+        'quiz-bgColor': '#F4E4C0',
+        black: '#20201D'
+      },
+      fontSize: {
+        'title-size': '118px',
+        'settingButton-size': '85px',
+        'compliment-size': '74px',
+        'resultButton-size': '36px'
+      },
+      lineHeight: {
+        title: '1.10'
+      },
+      padding: {
+        title: '100px'
+      },
+      letterSpacing: {
+        title: '2.8px'
+      }
+    },
+    backgroundImage: {
+      'chinese-frame': "url('../assets/chinese-frame.png')"
     }
   },
-  plugins: [],
-};
-
+  plugins: [require('daisyui')]
+}
