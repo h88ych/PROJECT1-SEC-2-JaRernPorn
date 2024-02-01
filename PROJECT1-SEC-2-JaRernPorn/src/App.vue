e09fe0129482a10c3cb038049392d7acdd947d51
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-02-02 01:36:48 +0700

    update event

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -1,9 +1,33 @@
 <script setup>
+import { reactive } from 'vue';
+
+//all page will add to this 
+const allPage = reactive({
+  homePage: true,
+  categoryPage: "",
+});
+
+//play button click event
+const playButton = () => {
+  allPage.homePage = false;
+  allPage.categoryPage = true;
+}
+
+//backToHome button click event
+const backToHome = () => {
+  allPage.homePage = true;
+  allPage.categoryPage = false;
+}
+
+
+
+
 </script>
 
 <template>
   <!-- HomePage -->
-  <div class="h-screen bg-main-bgColor p-5">
+  <section class="homePage" v-if="allPage.homePage">
+    <div class="h-screen bg-main-bgColor p-5">
     <div class="border-double border-8 border-title box-border h-full w-full">
       <div
         class="logo-title text-center text-title pt-title sm:text-title-size mobile:text-mobile-title-size"
@@ -18,11 +42,13 @@
       </div>
 
       <div id="home-page-button">
-        <div id="play-button" class="flex justify-center pt-12">
+        <!-- Play Button -->
+        <div id="play-button" class="flex justify-center pt-12" >
           <img
             class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out"
             src="./assets/home-page/play-button.svg"
             alt="play-button"
+            @click="playButton"
           />
         </div>
         <div id="HTP-button" class="flex justify-center pt-6">
@@ -35,15 +61,19 @@
       </div>
     </div>
   </div>
+  </section>
+  
 
   <!-- CategoryPage -->
-  <section class="category">
+  <section class="category" v-else-if="allPage.categoryPage">
     <div class="p-7 bg-main-bgColor h-screen overflow-y-hidden">
       <header>
+        <!-- Back to home Button -->
         <img
           class="w-16 absolute hover:w-catePage-20 transition-all duration-300 ease-in-out"
           src="./assets/categories/icon/left-arrow-01.png"
           alt="left-arrow"
+          @click="backToHome"
         />
         <div class="header flex justify-center items-center">
           <div
