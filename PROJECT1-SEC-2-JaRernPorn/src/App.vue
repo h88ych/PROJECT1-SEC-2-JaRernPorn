42254ebb9d50c738c3fc87d5be3e7b65162d074a
h88ych <hathaitat88@gmail.com>
2024-02-05 00:11:45 +0700

    WrongPopup
    Still no responsive

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -1,13 +1,40 @@
-<script setup>
+<script setup></script>
 
-</script>
- 
 <template>
-<div>
-
-</div>
+  <div id="body" class="bg-main-bgColor w-full h-screen p-5 relative">
+    <div
+      class="w-full h-full relative text-center flex justify-center items-center"
+    >
+      <div class="text-center relative translate-middle text-white">
+        <img
+          src="./assets/wrongPopup/Frame.png"
+          alt="Frame chinese theme"
+          class="mx-auto my-60"
+        />
+        <div
+          class="absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 flex-col space-y-14"
+        >
+          <h1
+            class="uppercase font-outfit mt-10 font-semibold text-wrongPopup-size"
+          >
+            wrong
+          </h1>
+          <p
+            class="uppercase text-2xl font-outfit md:text-resultButton-size sm:text-4xl xl:text-resultButton-size"
+          >
+            The answer is<br />
+            <span class="text-answer-fontColor">西瓜 (xīguā)</span>
+          </p>
+          <div class="border-b w-[770px] mx-auto relative -top-[15px]"></div>
+          <button
+            class="border border-10 w-60 h-24 text-[50px] rounded-full bg-white text-black font-semibold relative -top-[22px] shadow-md shadow-zinc-600"
+          >
+            NEXT
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
- 
-<style scoped>
 
-</style>
+<style scoped></style>
