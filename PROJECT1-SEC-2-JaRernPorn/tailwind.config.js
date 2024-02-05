49f473782a4d62ddf593de7f3593ec0bc5b25f29
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-02-05 23:46:46 +0700

    dev-HTP-page

--- PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
+++ PROJECT1-SEC-2-JaRernPorn/tailwind.config.js
@@ -3,6 +3,25 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        alkatra: ["Alkatra", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        title: "#B11717",
+        "button-bgColor": "#861616",
+        "main-bgColor": "#FDFFE8",
+        white: "#FFFFFF",
+        "quiz-bgColor": "#F4E4C0",
+        black: "#20201D",
+      },
+      screens:{
+        mobile:'320px',
+        sm:'480px', //phone
+        md:'768px',
+        lg:'1024px',
+        xl:'1440px' //desktop
+      }
     }
   },
   plugins: [],
