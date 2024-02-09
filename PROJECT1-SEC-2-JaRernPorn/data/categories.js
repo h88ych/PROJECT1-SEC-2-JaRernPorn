44aceab7835a393fcc41e10659517cbc810968ec
ONTTHDO <111364693+ONTTHDO@users.noreply.github.com>
2024-02-09 09:31:51 +0700

    update data

---
+++ PROJECT1-SEC-2-JaRernPorn/data/categories.js
@@ -0,0 +1,74 @@
+const images = [
+  {
+    categoryName: 'Fruits',
+    cateImg: 'src/assets/categories/cate_fruits.png',
+    groups: [
+      { id: 1, src: 'src/assets/play-game/fruits/apple-01.png', word: '苹果 (píngguǒ)' },
+      { id: 2, src: 'src/assets/play-game/fruits/banana-01.png', word: '香蕉 (xiāngjiāo)' },
+      { id: 3, src: 'src/assets/play-game/fruits/mango-01.png', word: '芒果 (mángguǒ)' },
+      { id: 4, src: 'src/assets/play-game/fruits/grape-01.png', word: '葡萄 (pú táo)' },
+      { id: 5, src: 'src/assets/play-game/fruits/pineapple-01.png', word: '菠萝 (bōluó)' },
+      { id: 6, src: 'src/assets/play-game/fruits/strawberry-01.png', word: '草莓 (cǎoméi)' },
+      { id: 7, src: 'src/assets/play-game/fruits/watermelon-01.png', word: '西瓜 (xīguā)' },
+    ]
+  },
+
+  {
+    categoryName: 'Animals',
+    cateImg: 'src/assets/categories/cate_animals.png',
+    groups: [
+      { id: 1, src: 'src/assets/play-game/animal/cat-01.png', word: '猫 (Māo)' },
+      { id: 2, src: 'src/assets/play-game/animal/dog-01.png', word: '狗 (gǒu)' },
+      { id: 3, src: 'src/assets/play-game/animal/elephat-01.png', word: '大象 (dà xiàng)' },
+      { id: 4, src: 'src/assets/play-game/animal/lion-01.png', word: '狮子	(Shīzi)' },
+      { id: 5, src: 'src/assets/play-game/animal/monkey-01.png', word: '猴子 (hóuzi)' },
+      { id: 6, src: 'src/assets/play-game/animal/rabbit-01.png', word: '兔子 (Tùzǐ)' },
+      { id: 7, src: 'src/assets/play-game/animal/tiger-01.png', word: '老虎	(Lǎohǔ)' },
+    ]
+  },
+
+  {
+    categoryName: 'Colors',
+    cateImg: 'src/assets/categories/cate_colors.png',
+    groups: [
+      { id: 1, src: 'src/assets/play-game/color/black-01.png', word: '黑色 (hēi sè)' },
+      { id: 2, src: 'src/assets/play-game/color/blue-01.png', word: '蓝色 (lán sè)' },
+      { id: 3, src: 'src/assets/play-game/color/green-01.png', word: '绿色 (lǜ sè)' },
+      { id: 4, src: 'src/assets/play-game/color/orange-01.png', word: '橙色 (chéng sè)' },
+      { id: 5, src: 'src/assets/play-game/color/pink-01.png', word: '粉红色 (fěn hóng sè)' },
+      { id: 6, src: 'src/assets/play-game/color/purple-01.png', word: '紫色 (zǐ sè)' },
+      { id: 7, src: 'src/assets/play-game/color/yellow-01.png', word: '黄色 (huáng sè)' },
+    ]
+  },
+
+  {
+    categoryName: 'Family',
+    cateImg: 'src/assets/categories/cate_family.png',
+    groups: [
+      { id: 1, src: 'src/assets/play-game/family/grandfather.png', word: '爷爷 (yé ye)' },
+      { id: 2, src: 'src/assets/play-game/family/mother.png', word: '妈妈 (mā ma)' },
+      { id: 3, src: 'src/assets/play-game/family/father.png', word: '爸爸 (bà ba)' },
+      { id: 4, src: 'src/assets/play-game/family/older-brother.png ', word: '哥哥 (gē ge)' },
+      { id: 5, src: 'src/assets/play-game/family/younger-brother.png', word: '弟弟 (dì di)' },
+      { id: 6, src: 'src/assets/play-game/family/older-sister.png', word: '姐姐 (jiě jie)' },
+      { id: 7, src: 'src/assets/play-game/family/younger-sister.png', word: '妹妹 (mèi mei)' },
+    ]
+  },
+
+  {
+    categoryName: 'Numbers',
+    cateImg: 'src/assets/categories/cate_numbers.png' ,
+    groups: [
+      { id: 1, src: 'src/assets/play-game/seven-01.png', word: '七 (qī)' },
+      { id: 2, src: 'src/assets/play-game/two-01.png', word: '二 (èr)' },
+      { id: 3, src: 'src/assets/play-game/five-01.png', word: '五 (wǔ)' },
+      { id: 4, src: 'src/assets/play-game/zero-01.png', word: '零 (líng)' },
+      { id: 5, src: 'src/assets/play-game/one-01.png', word: '一 (yī)' },
+      { id: 6, src: 'src/assets/play-game/ten-01.png', word: '十 (shí)' },
+      { id: 7, src: 'src/assets/play-game/eight-01.png', word: '八 (bā)' },
+    ]
+  }
+]
+
+export default images
+
