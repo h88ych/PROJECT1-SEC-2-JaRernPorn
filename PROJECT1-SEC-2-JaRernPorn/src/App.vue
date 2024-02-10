c9ff75ffce5cc0ef72a4fb21898be3f98c2482d2
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2024-02-10 20:41:03 +0700

    fix some data

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { reactive, ref, computed } from 'vue'
-import images from '../data/categories';
+import images from '../data/categories'
 
 const answer = ref('')
 const showResult = ref(false)
@@ -12,14 +12,14 @@ const currentIndexCate = ref(0)
 //all page will add to this
 const allPage = reactive({
   categoryPage: true,
-  playgamePage: "",
-});
+  playgamePage: ''
+})
 
 const showPlaygame = (index) => {
   currentIndexCate.value = index
-  allPage.playgamePage = true;
-  allPage.categoryPage = false;
-};
+  allPage.playgamePage = true
+  allPage.categoryPage = false
+}
 
 const currentCategory = computed(() => {
   return images[currentIndexCate.value].categoryName
@@ -27,7 +27,6 @@ const currentCategory = computed(() => {
 
 // รูปที่แสดงตอนเล่น
 const currentImage = computed(() => {
-
   if (currentIndex.value === images[currentIndexCate.value].groups.length) {
     return '' // เกมสิ้นสุด
   }
@@ -35,7 +34,6 @@ const currentImage = computed(() => {
   answer.value = images[currentIndexCate.value].groups[currentIndex.value].word
   options.value = generateOptions(answer.value)
   return images[currentIndexCate.value].groups[currentIndex.value].src
-
 })
 
 // สร้างตัวเลือก
@@ -43,12 +41,14 @@ const generateOptions = (answer) => {
   const optionsArray = []
   optionsArray.push({ id: 1, value: answer })
   while (optionsArray.length < 4) {
-    const randomWord = images[currentIndexCate.value].groups[Math.floor(Math.random() * images[currentIndexCate.value].groups.length)].word
+    const randomWord =
+      images[currentIndexCate.value].groups[
+        Math.floor(Math.random() * images[currentIndexCate.value].groups.length)
+      ].word
 
-    if (!optionsArray.some(option => option.value === randomWord)) {
+    if (!optionsArray.some((option) => option.value === randomWord)) {
       optionsArray.push({ id: optionsArray.length + 1, value: randomWord })
     }
-
   }
   return shuffle(optionsArray)
 }
@@ -56,8 +56,8 @@ const generateOptions = (answer) => {
 // สลับลำดับ
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
   }
   return array
 }
@@ -72,90 +72,121 @@ const checkAnswer = (selectedOption) => {
 
   if (currentIndex.value === images[currentIndexCate.value].groups.length - 1) {
     setTimeout(() => {
-      resultMessage.value = 'Game Over!';
-    }, 2000); // รอ 2 วิ ค่อยขึ้นจบเกม
-    return resultMessage.value;
+      resultMessage.value = 'Game Over!'
+    }, 2000) // รอ 2 วิ ค่อยขึ้นจบเกม
+    return resultMessage.value
   }
 
   setTimeout(() => {
     currentIndex.value++ // เลื่อนไปรูปถัดไป
     showResult.value = false // ไม่แสดงผลลัพธ์จากข้อที่แล้ว
-  }, 2000); // รอ 2 วิ
-
+  }, 2000) // รอ 2 วิ
 }
-
 </script>
 
 <template>
   <section class="category" v-if="allPage.categoryPage">
     <div class="p-7 bg-main-bgColor h-screen overflow-y-hidden">
-
       <header>
-        <img class="w-16 absolute hover:w-catePage-20 transition-all duration-300 ease-in-out"
-          src="./assets/categories/icon/left-arrow-01.png" alt="left-arrow" />
+        <img
+          class="w-16 absolute hover:w-catePage-20 transition-all duration-300 ease-in-out"
+          src="./assets/categories/icon/left-arrow-01.png"
+          alt="left-arrow"
+        />
 
         <div class="header flex justify-center items-center">
           <div
-            class="categories text-title font-semibold font-outfit text-5xl flex items-center justify-center w-full px-17">
+            class="categories text-title font-semibold font-outfit text-5xl flex items-center justify-center w-full px-17"
+          >
             Categories
           </div>
         </div>
       </header>
 
       <div class="allbox flex content-center justify-center mt-14">
-        <div class="boxes font-semibold text-black font-outfit flex flex-col flex-wrap">
-          <div class="firstLine flex flex-col md:flex-row space-x-0 md:space-x-28 pb-10 md:flex-wrap">
-
-
-            <div v-for="(category, categoryIndex) in images" :key="category.categoryName"
-              class="category-item flex flex-col items-center" @click="showPlaygame(categoryIndex)">
-
-              <div class="pic w-52 pb-2 hover:w-56 transition-all duration-300 ease-in-out">
-                <img :src="category.cateImg" :alt="category.categoryName" class="hover:drop-shadow-lg" />
+        <div
+          class="boxes font-semibold text-black font-outfit flex flex-col flex-wrap"
+        >
+          <div
+            class="firstLine flex flex-col md:flex-row space-x-0 md:space-x-28 pb-10 md:flex-wrap"
+          >
+            <div
+              v-for="(category, categoryIndex) in images"
+              :key="category.categoryName"
+              class="category-item flex flex-col items-center"
+              @click="showPlaygame(categoryIndex)"
+            >
+              <div
+                class="pic w-52 pb-2 hover:w-56 transition-all duration-300 ease-in-out"
+              >
+                <img
+                  :src="category.cateImg"
+                  :alt="category.categoryName"
+                  class="hover:drop-shadow-lg"
+                />
               </div>
 
               <p class="text-xl">{{ category.categoryName }}</p>
             </div>
-
           </div>
         </div>
       </div>
     </div>
   </section>
 
-
   <section class="playGame" v-else-if="allPage.playgamePage">
     <div id="app" class="mx-auto max-w-screen-lg">
-
-      <div class="-z-10 absolute ">
-        <img src="./assets/play-game/background.png" alt="background" class="h-screen bg-cover bg-center">
+      <div class="-z-10 absolute">
+        <img
+          src="./assets/play-game/background.png"
+          alt="background"
+          class="h-screen bg-cover bg-center"
+        />
       </div>
 
       <div class="header flex justify-between items-center py-4">
         <div class="play ml-4 absolute left-10 top-5">
-          <h1 class="text-title text-5xl font-outfit font-bold">Category:{{ currentCategory }} </h1>
+          <h1 class="text-title text-5xl font-outfit font-bold">
+            Category:{{ currentCategory }}
+          </h1>
         </div>
 
         <div class="setting mr-4 absolute right-10 top-6 hover:scale-125">
-          <img src="./assets/play-game/setting.png" alt="setting-image" class="w-12 " />
+          <img
+            src="./assets/play-game/setting.png"
+            alt="setting-image"
+            class="w-12"
+          />
         </div>
       </div>
 
       <div id="bg-image" class="flex justify-center my-12">
-        <img class="w-96 my-8" src="./assets/play-game/bg-image.png" alt="bg-image" />
-        <img class="absolute font-outfit mb-4 mx-8 my-20 size-48" :src="currentImage" :alt="answer" />
+        <img
+          class="w-96 my-8"
+          src="./assets/play-game/bg-image.png"
+          alt="bg-image"
+        />
+        <img
+          class="absolute font-outfit mb-4 mx-8 my-20 size-48"
+          :src="currentImage"
+          :alt="answer"
+        />
       </div>
 
       <div class="grid grid-cols-2 gap-4 justify-center">
-        <button v-for="(option, index) in options" :key="index" @click="checkAnswer(option.value)"
-          class="bg-button-bgColor text-white font-normal text-2xl py-3 px-6 h-20 rounded-md">{{ option.value }}</button>
+        <button
+          v-for="(option, index) in options"
+          :key="index"
+          @click="checkAnswer(option.value)"
+          class="bg-button-bgColor text-white font-normal text-2xl py-3 px-6 h-20 rounded-md"
+        >
+          {{ option.value }}
+        </button>
       </div>
 
       <p class="mt-4" v-if="showResult">{{ resultMessage }}</p>
-
     </div>
   </section>
 </template>
 
- 
 <style scoped></style>
