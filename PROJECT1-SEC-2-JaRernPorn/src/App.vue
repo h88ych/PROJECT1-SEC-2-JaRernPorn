8490416a04aac895706590027600df8abf91070c
h88ych <hathaitat88@gmail.com>
2024-02-10 21:08:01 +0700

    Add settingPopup

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
-
 import { reactive, ref, computed } from 'vue'
-import images from '../data/categories';
+import images from '../data/categories'
 
 const answer = ref('')
 const showResult = ref(false)
@@ -9,6 +8,7 @@ const resultMessage = ref('')
 const options = ref([])
 const currentIndex = ref(0)
 const currentIndexCate = ref(0)
+const settingButton = ref(false)
 
 //all page will add to this
 const allPage = reactive({
@@ -48,13 +48,22 @@ const showPlaygame = (index) => {
   allPage.categoryPage = false
 }
 
+//settingButton click event
+const openSetting = () => {
+  settingButton.value = true
+}
+
+//closeSettingButton click event
+const closeSetting = () => {
+  settingButton.value = false
+}
+
 const currentCategory = computed(() => {
   return images[currentIndexCate.value].categoryName
 })
 
 // รูปที่แสดงตอนเล่น
 const currentImage = computed(() => {
-
   if (currentIndex.value === images[currentIndexCate.value].groups.length) {
     return '' // เกมสิ้นสุด
   }
@@ -62,7 +71,6 @@ const currentImage = computed(() => {
   answer.value = images[currentIndexCate.value].groups[currentIndex.value].word
   options.value = generateOptions(answer.value)
   return images[currentIndexCate.value].groups[currentIndex.value].src
-
 })
 
 // สร้างตัวเลือก
@@ -70,12 +78,14 @@ const generateOptions = (answer) => {
   const optionsArray = []
   optionsArray.push({ id: 1, value: answer })
   while (optionsArray.length < 4) {
-    const randomWord = images[currentIndexCate.value].groups[Math.floor(Math.random() * images[currentIndexCate.value].groups.length)].word
+    const randomWord =
+      images[currentIndexCate.value].groups[
+        Math.floor(Math.random() * images[currentIndexCate.value].groups.length)
+      ].word
 
-    if (!optionsArray.some(option => option.value === randomWord)) {
+    if (!optionsArray.some((option) => option.value === randomWord)) {
       optionsArray.push({ id: optionsArray.length + 1, value: randomWord })
     }
-
   }
   return shuffle(optionsArray)
 }
@@ -83,8 +93,8 @@ const generateOptions = (answer) => {
 // สลับลำดับ
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
   }
   return array
 }
@@ -99,27 +109,29 @@ const checkAnswer = (selectedOption) => {
 
   if (currentIndex.value === images[currentIndexCate.value].groups.length - 1) {
     setTimeout(() => {
-      resultMessage.value = 'Game Over!';
-    }, 2000); // รอ 2 วิ ค่อยขึ้นจบเกม
-    return resultMessage.value;
+      resultMessage.value = 'Game Over!'
+    }, 2000) // รอ 2 วิ ค่อยขึ้นจบเกม
+    return resultMessage.value
   }
 
   setTimeout(() => {
     currentIndex.value++ // เลื่อนไปรูปถัดไป
     showResult.value = false // ไม่แสดงผลลัพธ์จากข้อที่แล้ว
-  }, 2000); // รอ 2 วิ
-
+  }, 2000) // รอ 2 วิ
 }
-
-
 </script>
 
 <template>
   <!-- Home Page -->
   <section class="homePage" v-if="allPage.homePage">
-    <div class="h-screen w-full bg-main-bgColor p-5" :class="{ absolute: allPage.isPopUp }">
+    <div
+      class="h-screen w-full bg-main-bgColor p-5"
+      :class="{ absolute: allPage.isPopUp }"
+    >
       <div class="border-double border-8 border-title box-border h-full w-full">
-        <div class="logo-title text-center text-title pt-title sm:text-title-size mobile:text-mobile-title-size">
+        <div
+          class="logo-title text-center text-title pt-title sm:text-title-size mobile:text-mobile-title-size"
+        >
           <div class="font-alkatra font-medium leading-title">HELLO !</div>
           <div class="font-alkatra font-medium leading-title">CHINESE</div>
         </div>
@@ -132,43 +144,69 @@ const checkAnswer = (selectedOption) => {
         <div id="home-page-button">
           <!-- Play Button -->
           <div id="play-button" class="flex justify-center pt-12">
-            <img class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
-              src="./assets/home-page/play-button.svg" alt="play-button" @click="playButton" />
+            <img
+              class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
+              src="./assets/home-page/play-button.svg"
+              alt="play-button"
+              @click="playButton"
+            />
           </div>
           <!-- How to Play Button -->
           <div id="HTP-button" class="flex justify-center pt-6">
-            <img class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
-              src="./assets/home-page/HTP-button.svg" alt="HTP-button" @click="howToPlayButton" />
+            <img
+              class="w-72 relative hover:w-80 transition-all duration-300 ease-in-out cursor-pointer"
+              src="./assets/home-page/HTP-button.svg"
+              alt="HTP-button"
+              @click="howToPlayButton"
+            />
           </div>
         </div>
       </div>
     </div>
 
     <!-- How To Play Page -->
-    <div class="flex justify-center items-center h-screen" v-if="allPage.isPopUp">
+    <div
+      class="flex justify-center items-center h-screen"
+      v-if="allPage.isPopUp"
+    >
       <div
-        class="overflow-y-scroll h-4/5 bg-white rounded-2xl drop-shadow-2xl relative md:w-5/12 sm:w-6/12 mobile:w-11/12">
+        class="overflow-y-scroll h-4/5 bg-white rounded-2xl drop-shadow-2xl relative md:w-5/12 sm:w-6/12 mobile:w-11/12"
+      >
         <!-- Close Button -->
         <div class="sticky top-3 cursor-pointer" @click="close">
-          <img class="w-6 sticky left-3" title="close" src="./assets/HowToPlay-page/close-vector.png"
-            alt="close-vector" />
+          <img
+            class="w-6 sticky left-3"
+            title="close"
+            src="./assets/HowToPlay-page/close-vector.png"
+            alt="close-vector"
+          />
         </div>
         <div class="flex justify-center items-center">
           <div class="w-3/4">
-            <h1 class="text-4xl font-bold text-center mb-6 font-alkatra text-title">
+            <h1
+              class="text-4xl font-bold text-center mb-6 font-alkatra text-title"
+            >
               HOW TO PLAY
             </h1>
             <div class="content">
               <div class="HTP-1">
                 <p class="text-lg mb-4 font-outfit">1. Select the category.</p>
-                <img class="mb-9" src="./assets/HowToPlay-page/HTP-1.png" alt="HTP-1" />
+                <img
+                  class="mb-9"
+                  src="./assets/HowToPlay-page/HTP-1.png"
+                  alt="HTP-1"
+                />
               </div>
 
               <div class="HTP-2">
                 <p class="text-lg mb-4 font-outfit">
                   2. Choose the word that matches the picture.
                 </p>
-                <img class="mb-9" src="./assets/HowToPlay-page/HTP-2.png" alt="HTP-2" />
+                <img
+                  class="mb-9"
+                  src="./assets/HowToPlay-page/HTP-2.png"
+                  alt="HTP-2"
+                />
               </div>
 
               <div class="HTP-3">
@@ -178,7 +216,11 @@ const checkAnswer = (selectedOption) => {
                   appear with the correct answer before moving on to the next
                   question.
                 </p>
-                <img class="mb-9 w-screen rounded-lg" src="./assets/HowToPlay-page/HTP-3.png" alt="HTP-3" />
+                <img
+                  class="mb-9 w-screen rounded-lg"
+                  src="./assets/HowToPlay-page/HTP-3.png"
+                  alt="HTP-3"
+                />
               </div>
 
               <div class="HTP-4">
@@ -186,7 +228,11 @@ const checkAnswer = (selectedOption) => {
                   4. Game end: List displays your choice and correct answer.
                   Correct is normal, incorrect is red.
                 </p>
-                <img class="mb-9" src="./assets/HowToPlay-page/HTP-4.png" alt="HTP-4" />
+                <img
+                  class="mb-9"
+                  src="./assets/HowToPlay-page/HTP-4.png"
+                  alt="HTP-4"
+                />
               </div>
             </div>
           </div>
@@ -200,30 +246,46 @@ const checkAnswer = (selectedOption) => {
     <div class="p-7 bg-main-bgColor h-screen overflow-y-hidden">
       <header>
         <!-- Back to home Button -->
-        <img class="w-16 absolute hover:w-catePage-20 transition-all duration-300 ease-in-out cursor-pointer"
-          src="./assets/categories/icon/left-arrow-01.png" alt="left-arrow" @click="backToHome" />
+        <img
+          class="w-16 absolute hover:w-catePage-20 transition-all duration-300 ease-in-out cursor-pointer"
+          src="./assets/categories/icon/left-arrow-01.png"
+          alt="left-arrow"
+          @click="backToHome"
+        />
         <div class="header flex justify-center items-center">
           <div
-            class="categories text-title font-semibold font-outfit text-5xl flex items-center justify-center w-full px-17">
+            class="categories text-title font-semibold font-outfit text-5xl flex items-center justify-center w-full px-17"
+          >
             Categories
           </div>
         </div>
       </header>
 
       <div class="allbox flex content-center justify-center mt-14">
-        <div class="boxes font-semibold text-black font-outfit flex flex-col flex-wrap">
-          <div class="firstLine flex flex-col md:flex-row space-x-0 md:space-x-72 pb-10 md:flex-wrap md:ml-80">
-
-            <div v-for="(category, categoryIndex) in images" :key="category.categoryName"
-                class="category-item flex flex-col items-center" @click="showPlaygame(categoryIndex)">
-
-                <div class="pic w-52 pb-2 hover:w-56 transition-all duration-300 ease-in-out">
-                  <img :src="category.cateImg" :alt="category.categoryName" class="hover:drop-shadow-lg" />
-                </div>
-
-                <p class="text-xl">{{ category.categoryName }}</p>
+        <div
+          class="boxes font-semibold text-black font-outfit flex flex-col flex-wrap"
+        >
+          <div
+            class="firstLine flex flex-col md:flex-row space-x-0 md:space-x-72 pb-10 md:flex-wrap md:ml-80"
+          >
+            <div
+              v-for="(category, categoryIndex) in images"
+              :key="category.categoryName"
+              class="category-item flex flex-col items-center"
+              @click="showPlaygame(categoryIndex)"
+            >
+              <div
+                class="pic w-52 pb-2 hover:w-56 transition-all duration-300 ease-in-out"
+              >
+                <img
+                  :src="category.cateImg"
+                  :alt="category.categoryName"
+                  class="hover:drop-shadow-lg"
+                />
               </div>
 
+              <p class="text-xl">{{ category.categoryName }}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -233,37 +295,107 @@ const checkAnswer = (selectedOption) => {
   <!-- Play Game Page -->
   <section class="playGame" v-else-if="allPage.playgamePage">
     <div id="app" class="mx-auto max-w-screen-lg">
-
-      <div class="-z-10 absolute ">
-        <img src="./assets/play-game/background.png" alt="background" class="h-screen bg-cover bg-center">
+      <div class="-z-10 absolute">
+        <img
+          src="./assets/play-game/background.png"
+          alt="background"
+          class="h-screen bg-cover bg-center"
+        />
       </div>
+
+      <section
+        id="settingButton"
+        v-if="settingButton"
+        class="fixed inset-0 z-50"
+      >
+        <div
+          class="w-full h-screen flex justify-center items-center bg-black bg-opacity-50"
+        >
+          <div class="relative">
+            <div
+              class="rounded-3xl bg-white bg-opacity-100 w-[200px] h-[403px] border border-1 border-black sm:w-[300px] md:w-[450px] md:h-[403px]"
+            >
+              <div class="flex-col p-5">
+                <img
+                  src="./assets/setting-features/cancel.png"
+                  alt="cancel button"
+                  class="absolute right-4 top-5 w-5 sm:w-8 cursor-pointer"
+                  @click="closeSetting"
+                />
+
+                <div
+                  class="text-center text-2xl font-alkatra text-title font-semibold sm:text-6xl md:text-settingButton-size"
+                  style="text-shadow: 2px 2px 4px rgba(18, 17, 17, 0.5)"
+                >
+                  Setting
+                </div>
+
+                <div class="flex flex-col items-center gap-4 mt-4">
+                  <img
+                    src="./assets/setting-features/resumeButton.png"
+                    alt="resume button"
+                    class="resume button"
+                  />
+                  <img
+                    src="./assets/setting-features/restartButton.png"
+                    alt="restart button"
+                    class="mb-2"
+                  />
+                  <img
+                    src="./assets/setting-features/homebutton.png"
+                    alt="home button"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <div class="header flex justify-between items-center py-4">
         <div class="play ml-4 absolute left-10 top-5">
-          <h1 class="text-title text-5xl font-outfit font-bold">Category:{{ currentCategory }} </h1>
+          <h1 class="text-title text-5xl font-outfit font-bold">
+            Category:{{ currentCategory }}
+          </h1>
         </div>
 
         <div class="setting mr-4 absolute right-10 top-6 hover:scale-125">
-          <img src="./assets/play-game/setting.png" alt="setting-image" class="w-12 " />
-
+          <img
+            src="./assets/play-game/setting.png"
+            alt="setting-image"
+            class="w-12"
+            @click="openSetting"
+          />
         </div>
       </div>
 
       <div id="bg-image" class="flex justify-center my-12">
-        <img class="w-96 my-8" src="./assets/play-game/bg-image.png" alt="bg-image" />
-        <img class="absolute font-outfit mb-4 mx-8 my-20 size-48" :src="currentImage" :alt="answer" />
+        <img
+          class="w-96 my-8"
+          src="./assets/play-game/bg-image.png"
+          alt="bg-image"
+        />
+        <img
+          class="absolute font-outfit mb-4 mx-8 my-20 size-48"
+          :src="currentImage"
+          :alt="answer"
+        />
       </div>
 
       <div class="grid grid-cols-2 gap-4 justify-center">
-        <button v-for="(option, index) in options" :key="index" @click="checkAnswer(option.value)"
-          class="bg-button-bgColor text-white font-normal text-2xl py-3 px-6 h-20 rounded-md">{{ option.value }}</button>
+        <button
+          v-for="(option, index) in options"
+          :key="index"
+          @click="checkAnswer(option.value)"
+          class="bg-button-bgColor text-white font-normal text-2xl py-3 px-6 h-20 rounded-md"
+        >
+          {{ option.value }}
+        </button>
       </div>
 
       <p class="mt-4" v-if="showResult">{{ resultMessage }}</p>
-
     </div>
   </section>
+</template>
 
-  </template>
-
-  <style scoped></style>
+<style scoped></style>
