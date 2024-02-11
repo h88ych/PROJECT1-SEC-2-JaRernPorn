a0ec45af008b5d2d13f46cc09922d18f98a8532a
h88ych <hathaitat88@gmail.com>
2024-02-11 23:46:15 +0700

    Edit data meaning

--- PROJECT1-SEC-2-JaRernPorn/data/categories.js
+++ PROJECT1-SEC-2-JaRernPorn/data/categories.js
@@ -1,248 +1,248 @@
 const images = [
   {
-    categoryName: "Fruits",
-    cateImg: "src/assets/categories/cate_fruits.png",
+    categoryName: 'Fruits',
+    cateImg: 'src/assets/categories/cate_fruits.png',
     groups: [
       {
         id: 1,
-        src: "src/assets/play-game/fruits/apple-01.png",
-        word: "苹果 (píngguǒ)",
-        meaning: "Apple",
+        src: 'src/assets/play-game/fruits/apple-01.png',
+        word: '苹果 (píngguǒ)',
+        meaning: 'Apple'
       },
       {
         id: 2,
-        src: "src/assets/play-game/fruits/banana-01.png",
-        word: "香蕉 (xiāngjiāo)",
-        meaning: "Banana",
+        src: 'src/assets/play-game/fruits/banana-01.png',
+        word: '香蕉 (xiāngjiāo)',
+        meaning: 'Banana'
       },
       {
         id: 3,
-        src: "src/assets/play-game/fruits/mango-01.png",
-        word: "芒果 (mángguǒ)",
-        meaning: "Mango",
+        src: 'src/assets/play-game/fruits/mango-01.png',
+        word: '芒果 (mángguǒ)',
+        meaning: 'Mango'
       },
       {
         id: 4,
-        src: "src/assets/play-game/fruits/grape-01.png",
-        word: "葡萄 (pú táo)",
-        meaning: "Grape",
+        src: 'src/assets/play-game/fruits/grape-01.png',
+        word: '葡萄 (pú táo)',
+        meaning: 'Grape'
       },
       {
         id: 5,
-        src: "src/assets/play-game/fruits/pineapple-01.png",
-        word: "菠萝 (bōluó)",
-        meaning: "Pineapple",
+        src: 'src/assets/play-game/fruits/pineapple-01.png',
+        word: '菠萝 (bōluó)',
+        meaning: 'Pineapple'
       },
       {
         id: 6,
-        src: "src/assets/play-game/fruits/strawberry-01.png",
-        word: "草莓 (cǎoméi)",
-        meaning: "Strawberry",
+        src: 'src/assets/play-game/fruits/strawberry-01.png',
+        word: '草莓 (cǎoméi)',
+        meaning: 'Strawberry'
       },
       {
         id: 7,
-        src: "src/assets/play-game/fruits/watermelon-01.png",
-        word: "西瓜 (xīguā)",
-        meaning: "Watermelon",
-      },
-    ],
+        src: 'src/assets/play-game/fruits/watermelon-01.png',
+        word: '西瓜 (xīguā)',
+        meaning: 'Watermelon'
+      }
+    ]
   },
 
   {
-    categoryName: "Animals",
-    cateImg: "src/assets/categories/cate_animals.png",
+    categoryName: 'Animals',
+    cateImg: 'src/assets/categories/cate_animals.png',
     groups: [
       {
         id: 1,
-        src: "src/assets/play-game/animal/cat-01.png",
-        word: "猫 (Māo)",
-        meaning: "Cat",
+        src: 'src/assets/play-game/animal/cat-01.png',
+        word: '猫 (Māo)',
+        meaning: 'Cat'
       },
       {
         id: 2,
-        src: "src/assets/play-game/animal/dog-01.png",
-        word: "狗 (gǒu)",
-        meaning: "dog",
+        src: 'src/assets/play-game/animal/dog-01.png',
+        word: '狗 (gǒu)',
+        meaning: 'dog'
       },
       {
         id: 3,
-        src: "src/assets/play-game/animal/elephat-01.png",
-        word: "大象 (dà xiàng)",
-        meaning: "Elephant",
+        src: 'src/assets/play-game/animal/elephat-01.png',
+        word: '大象 (dà xiàng)',
+        meaning: 'Elephant'
       },
       {
         id: 4,
-        src: "src/assets/play-game/animal/lion-01.png",
-        word: "狮子	(Shīzi)",
-        meaning: "Lion",
+        src: 'src/assets/play-game/animal/lion-01.png',
+        word: '狮子	(Shīzi)',
+        meaning: 'Lion'
       },
       {
         id: 5,
-        src: "src/assets/play-game/animal/monkey-01.png",
-        word: "猴子 (hóuzi)",
-        meaning: "Monkey",
+        src: 'src/assets/play-game/animal/monkey-01.png',
+        word: '猴子 (hóuzi)',
+        meaning: 'Monkey'
       },
       {
         id: 6,
-        src: "src/assets/play-game/animal/rabbit-01.png",
-        word: "兔子 (Tùzǐ)",
-        meaning: "Rabbit",
+        src: 'src/assets/play-game/animal/rabbit-01.png',
+        word: '兔子 (Tùzǐ)',
+        meaning: 'Rabbit'
       },
       {
         id: 7,
-        src: "src/assets/play-game/animal/tiger-01.png",
-        word: "老虎	(Lǎohǔ)",
-        meaning: "Tiger",
-      },
-    ],
+        src: 'src/assets/play-game/animal/tiger-01.png',
+        word: '老虎	(Lǎohǔ)',
+        meaning: 'Tiger'
+      }
+    ]
   },
 
   {
-    categoryName: "Colors",
-    cateImg: "src/assets/categories/cate_colors.png",
+    categoryName: 'Colors',
+    cateImg: 'src/assets/categories/cate_colors.png',
     groups: [
       {
         id: 1,
-        src: "src/assets/play-game/color/black-01.png",
-        word: "黑色 (hēi sè)",
-        meaning: "Black",
+        src: 'src/assets/play-game/color/black-01.png',
+        word: '黑色 (hēi sè)',
+        meaning: 'Black'
       },
       {
         id: 2,
-        src: "src/assets/play-game/color/blue-01.png",
-        word: "蓝色 (lán sè)",
-        meaning: "Blue",
+        src: 'src/assets/play-game/color/blue-01.png',
+        word: '蓝色 (lán sè)',
+        meaning: 'Blue'
       },
       {
         id: 3,
-        src: "src/assets/play-game/color/green-01.png",
-        word: "绿色 (lǜ sè)",
-        meaning: "Green",
+        src: 'src/assets/play-game/color/green-01.png',
+        word: '绿色 (lǜ sè)',
+        meaning: 'Green'
       },
       {
         id: 4,
-        src: "src/assets/play-game/color/orange-01.png",
-        word: "橙色 (chéng sè)",
-        meaning: "Orange",
+        src: 'src/assets/play-game/color/orange-01.png',
+        word: '橙色 (chéng sè)',
+        meaning: 'Orange'
       },
       {
         id: 5,
-        src: "src/assets/play-game/color/pink-01.png",
-        word: "粉红色 (fěn hóng sè)",
-        meaning: "Apple",
+        src: 'src/assets/play-game/color/pink-01.png',
+        word: '粉红色 (fěn hóng sè)',
+        meaning: 'Pink'
       },
       {
         id: 6,
-        src: "src/assets/play-game/color/purple-01.png",
-        word: "紫色 (zǐ sè)",
-        meaning: "Pink",
+        src: 'src/assets/play-game/color/purple-01.png',
+        word: '紫色 (zǐ sè)',
+        meaning: 'Purple'
       },
       {
         id: 7,
-        src: "src/assets/play-game/color/yellow-01.png",
-        word: "黄色 (huáng sè)",
-        meaning: "Yellow",
-      },
-    ],
+        src: 'src/assets/play-game/color/yellow-01.png',
+        word: '黄色 (huáng sè)',
+        meaning: 'Yellow'
+      }
+    ]
   },
 
   {
-    categoryName: "Family",
-    cateImg: "src/assets/categories/cate_family.png",
+    categoryName: 'Family',
+    cateImg: 'src/assets/categories/cate_family.png',
     groups: [
       {
         id: 1,
-        src: "src/assets/play-game/family/grandfather.png",
-        word: "爷爷 (yé ye)",
-        meaning: "Grandfather",
+        src: 'src/assets/play-game/family/grandfather.png',
+        word: '爷爷 (yé ye)',
+        meaning: 'Grandfather'
       },
       {
         id: 2,
-        src: "src/assets/play-game/family/mother.png",
-        word: "妈妈 (mā ma)",
-        meaning: "Mother",
+        src: 'src/assets/play-game/family/mother.png',
+        word: '妈妈 (mā ma)',
+        meaning: 'Mother'
       },
       {
         id: 3,
-        src: "src/assets/play-game/family/father.png",
-        word: "爸爸 (bà ba)",
-        meaning: "Dad",
+        src: 'src/assets/play-game/family/father.png',
+        word: '爸爸 (bà ba)',
+        meaning: 'Father'
       },
       {
         id: 4,
-        src: "src/assets/play-game/family/older-brother.png ",
-        word: "哥哥 (gē ge)",
-        meaning: "Elder brother",
+        src: 'src/assets/play-game/family/older-brother.png ',
+        word: '哥哥 (gē ge)',
+        meaning: 'Elder brother'
       },
       {
         id: 5,
-        src: "src/assets/play-game/family/younger-brother.png",
-        word: "弟弟 (dì di)",
-        meaning: "Younger brother",
+        src: 'src/assets/play-game/family/younger-brother.png',
+        word: '弟弟 (dì di)',
+        meaning: 'Younger brother'
       },
       {
         id: 6,
-        src: "src/assets/play-game/family/older-sister.png",
-        word: "姐姐 (jiě jie)",
-        meaning: "Elder sister",
+        src: 'src/assets/play-game/family/older-sister.png',
+        word: '姐姐 (jiě jie)',
+        meaning: 'Elder sister'
       },
       {
         id: 7,
-        src: "src/assets/play-game/family/younger-sister.png",
-        word: "妹妹 (mèi mei)",
-        meaning: "Younger sister",
-      },
-    ],
+        src: 'src/assets/play-game/family/younger-sister.png',
+        word: '妹妹 (mèi mei)',
+        meaning: 'Younger sister'
+      }
+    ]
   },
 
   {
-    categoryName: "Numbers",
-    cateImg: "src/assets/categories/cate_numbers.png",
+    categoryName: 'Numbers',
+    cateImg: 'src/assets/categories/cate_numbers.png',
     groups: [
       {
         id: 1,
-        src: "src/assets/play-game/number/seven-01.png",
-        word: "七 (qī)",
-        meaning: "Seven",
+        src: 'src/assets/play-game/number/seven-01.png',
+        word: '七 (qī)',
+        meaning: 'Seven'
       },
       {
         id: 2,
-        src: "src/assets/play-game/number/two-01.png",
-        word: "二 (èr)",
-        meaning: "Two",
+        src: 'src/assets/play-game/number/two-01.png',
+        word: '二 (èr)',
+        meaning: 'Two'
       },
       {
         id: 3,
-        src: "src/assets/play-game/number/five-01.png",
-        word: "五 (wǔ)",
-        meaning: "Five",
+        src: 'src/assets/play-game/number/five-01.png',
+        word: '五 (wǔ)',
+        meaning: 'Five'
       },
       {
         id: 4,
-        src: "src/assets/play-game/number/zero-01.png",
-        word: "零 (líng)",
-        meaning: "Zero",
+        src: 'src/assets/play-game/number/zero-01.png',
+        word: '零 (líng)',
+        meaning: 'Zero'
       },
       {
         id: 5,
-        src: "src/assets/play-game/number/one-01.png",
-        word: "一 (yī)",
-        meaning: "One",
+        src: 'src/assets/play-game/number/one-01.png',
+        word: '一 (yī)',
+        meaning: 'One'
       },
       {
         id: 6,
-        src: "src/assets/play-game/number/ten-01.png",
-        word: "十 (shí)",
-        meaning: "Ten",
+        src: 'src/assets/play-game/number/ten-01.png',
+        word: '十 (shí)',
+        meaning: 'Ten'
       },
       {
         id: 7,
-        src: "src/assets/play-game/number/eight-01.png",
-        word: "八 (bā)",
-        meaning: "Eight",
-      },
-    ],
-  },
-];
+        src: 'src/assets/play-game/number/eight-01.png',
+        word: '八 (bā)',
+        meaning: 'Eight'
+      }
+    ]
+  }
+]
 
-export default images;
+export default images
