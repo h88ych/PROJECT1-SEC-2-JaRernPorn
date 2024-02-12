b1017df9083c7a3e9e7314f9a951ca53b88a7660
h88ych <hathaitat88@gmail.com>
2024-02-12 23:39:39 +0700

    Fix responsive bg popup

--- PROJECT1-SEC-2-JaRernPorn/src/App.vue
+++ PROJECT1-SEC-2-JaRernPorn/src/App.vue
@@ -505,39 +505,33 @@ const setButtonCorrect = (optionValue) => {
   <!-- Wrong popup  -->
   <section
     v-if="popUp.showAnswer"
-    class="absolute inset-0 flex justify-center items-center bg-black bg-opacity-75 z-50"
+    class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-75 z-50"
   >
-    <div id="body" class="w-full h-screen p-5 relative">
+    <div class="w-full h-full flex justify-center items-center">
+      <img
+        src="/wrongPopup/bear-frame.png"
+        alt="Frame chinese theme"
+        class="mx-auto mobile:w-96 lg:w-3/5 xl:w-6/12"
+      />
       <div
-        class="w-full h-full relative text-center flex justify-center items-center"
+        class="absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 flex-col space-y-14"
       >
-        <div class="text-center relative translate-middle text-white">
-          <img
-            src="/wrongPopup/bear-frame.png"
-            alt="Frame chinese theme"
-            class="mx-auto mobile:w-96 lg:w-3/5 xl:w-6/12"
-          />
+        <div
+          class="capitalize text-brownColor text-xl font-alkatra font-semibold tracking-wide relative right-7 top-8 sm:right-[50px] sm:text-4xl md:text-5xl lg:text-6xl lg:right-[60px] mobile:text-[25px]"
+        >
+          The answer is<br />
           <div
-            class="absolute p-4 text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 flex-col space-y-14"
+            class="text-answer-fontColor font-NotoSansSC font-medium lowercase text-sm relative sm:text-2xl md:text-4xl mobile:text-[20px]"
           >
-            <div
-              class="capitalize text-brownColor text-xl font-alkatra font-semibold tracking-wide relative right-7 top-8 sm:right-[50px] sm:text-4xl md:text-5xl lg:text-6xl lg:right-[60px] mobile:text-[25px]"
-            >
-              The answer is<br />
-              <div
-                class="text-answer-fontColor font-NotoSansSC font-medium lowercase text-sm relative sm:text-2xl md:text-4xl mobile:text-[20px]"
-              >
-                {{ answer }}
-              </div>
-            </div>
-            <button
-              class="text-sm text-brownColor font-semibold font-alkatra tracking-wider relative right-7 bottom-3 hover:text-answer-fontColor sm:right-[50px] sm:top-[18px] sm:text-2xl md:text-3xl lg:right-[60px] mobile:text-[20px]"
-              @click="handleNextBtn"
-            >
-              NEXT
-            </button>
+            {{ answer }}
           </div>
         </div>
+        <button
+          class="text-sm text-brownColor font-semibold font-alkatra tracking-wider relative right-7 bottom-3 hover:text-answer-fontColor sm:right-[50px] sm:top-[18px] sm:text-2xl md:text-3xl lg:right-[60px] mobile:text-[20px]"
+          @click="handleNextBtn"
+        >
+          NEXT
+        </button>
       </div>
     </div>
   </section>
