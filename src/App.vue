64a96c35b02d5c518c44c1a3232feb0c80193016
mg.apisara <111364885+mg-dotcom@users.noreply.github.com>
2024-02-15 19:56:17 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -509,7 +509,7 @@ const setButtonCorrect = (optionValue) => {
           </div>
         </div>
         <button
-          class="text-sm text-brownColor font-semibold font-alkatra tracking-wider relative right-7 bottom-3 hover:text-answer-fontColor sm:right-[50px] sm:top-[18px] sm:text-2xl md:text-3xl lg:right-[60px] mobile:text-[20px]"
+          class="text-sm text-brownColor font-semibold font-alkatra tracking-wider relative right-7 bottom-3 hover:text-answer-fontColor hove:transition duration-150 sm:right-[50px] sm:top-[18px] sm:text-2xl md:text-3xl lg:right-[60px] mobile:text-[20px]"
           @click="handleNextBtn"
         >
           NEXT
